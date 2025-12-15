4e2f106e260da57dc92eabc06546dbec8392b5d1
Abisoye <theabisoyelawal@gmail.com>
2025-12-15 13:58:32 +0100

    Fix task_4.sql: show full description of books table

--- task_4.sql
+++ task_4.sql
@@ -1,12 +1,2 @@
-SELECT
-    COLUMN_NAME,
-    COLUMN_TYPE,
-    IS_NULLABLE,
-    COLUMN_DEFAULT,
-    COLUMN_KEY,
-    EXTRA
-FROM
-    INFORMATION_SCHEMA.COLUMNS
-WHERE
-    TABLE_SCHEMA = 'alx_book_store'
-    AND TABLE_NAME = 'books';
+USE alx_book_store;
+SHOW COLUMNS FROM books;
